54ed87410a651ae24d46dc6dfd6406d2414db618
ashrafbel <ashrafmus2@gmail.com>
2023-06-15 19:16:52 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 /**
-* main - 4-puts.c
+*main - Entry point
+*
+*Description: Prints "Programming is like building a multilingual puzzle"
+*
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-       return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
