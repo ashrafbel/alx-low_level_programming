d2be5ffb1a70708388902c1c7dfddd9b9bcf7c76
ashrafbel <ashrafmus2@gimail.com>
2023-08-31 17:49:06 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,22 +5,21 @@
 * @b: A string containing the binary number.
 *
 * Return: The converted unsigned int value.
+*
 */
-unsigned int binary_to_uint_custom(const char *b)
+
+unsigned int binary_to_uint(const char *b)
 {
 int y;
 unsigned int dec_val = 0;
-
 if (!b)
 return (0);
-
 for (y = 0; b[y]; y++)
 {
 if (b[y] < '0' || b[y] > '1')
 return (0);
 dec_val = 2 * dec_val + (b[y] - '0');
 }
-
 return (dec_val);
 }
 
