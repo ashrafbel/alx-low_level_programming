300b45255768190a543710708cf787d05e5d962d
ashrafbel <ashrafmus2@gimail.com>
2023-09-04 16:29:15 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,29 +11,28 @@
  * @text_content: Text content to write to the file (can be NULL).
  * Return: 1 on success, -1 on failure.
  */
-int create_file(const char *filename, char *text_content)
+nt create_file(const char *filename, char *text_content)
+
 {
-    int fd, l = 0;
+	int fd, t, l = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
+	if (text_content != NULL)
+	{
+		for (l = 0; text_content[l];)
+			l++;
+	}
 
-    if (text_content != NULL)
-    {
-        while (text_content[l])
-            l++;
-        if (write(fd, text_content, l) == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	t = write(fd, text_content, l);
 
-    close(fd);
-    return (1);
+	if (fd == -1 || t == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
 
