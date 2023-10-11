a4c5647c8a6883d4c93ed2cbc9a38dcacfee84ba
ashrafBel <ashrafmus2@gmial.com>
2023-10-10 18:12:52 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog Structures
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: owner's dog
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
