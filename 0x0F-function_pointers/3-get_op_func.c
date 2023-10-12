8706ff2a7ec52b80e9a9e66b03f95856a09544c8
ashrafBel <ashrafmus2@gmial.com>
2023-10-11 17:10:39 -0700

    3-func

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * get_op_func - Identifies the appropriate function requested operation
+ * @s: Operator string.
+ * Return: Pointer to corresponding operation function or NULL if not found.
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL},
+};
+
+int i = 0;
+
+while (ops[i].op != NULL && *(ops[i].op) != *s)
+i++;
+
+return (ops[i].f);
+}
+
