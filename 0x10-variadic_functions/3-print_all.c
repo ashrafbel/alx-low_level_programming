86052b7e506acf15905bc80413373ebd61a14095
ashrafBel <ashrafmus2@gmial.com>
2023-10-12 18:42:17 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,47 +1,48 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
-
 /**
  * print_all - Print various data types from a format string.
  * @format: List of argument types.
  */
 void print_all(const char * const format, ...)
 {
-int k;
-char *str, *s = "";
+	int k = 0;
+	char *str, *s = "";
 
-va_list list;
+	va_list list;
 
-va_start(list, format);
+	va_start(list, format);
 
-if (format)
-{
-for (k = 0; format[k]; k++)
-{
-switch (format[k])
-{
-case 'c':
-printf("%s%c", s, va_arg(list, int));
-break;
-case 'i':
-printf("%s%d", s, va_arg(list, int));
+	if (format)
+	{
+		while (format[k])
+		{
+			switch (format[k])
+			{
+				case 'c':
+					printf("%s%c", s, va_arg(list, int));
 					break;
-case 'f':
-printf("%s%f", s, va_arg(list, double));
-break;
-case 's':
-str = va_arg(list, char *);
-if (!str)
-str = "(nil)";
-printf("%s%s", s, str);
-break;
-default:
-continue;
-}
-s = ", ";
-}
-}
+				case 'i':
+					printf("%s%d", s, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", s, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", s, str);
+					break;
+				default:
+					k++;
+					continue;
+			}
+			s = ", ";
+			k++;
+		}
+	}
 
 	printf("\n");
 	va_end(list);
