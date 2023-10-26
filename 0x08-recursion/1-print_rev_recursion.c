0ad994484b76948988fb3895a22b7f7e7eb1a4b0
ashrafbel <ashrafmus2@gmial.com>
2023-10-26 13:08:13 -0700

    1-print_rev_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,14 +2,14 @@
 
 
 /**
- * print_rev_recursion - print the string with reverse
+ *_print_rev_recursion - print the string with reverse
  * @s: the pointer of string
  * Return: viod
  */
 void _print_rev_recursion(char *s)
-{       
+{
 if (*s != '\0')
-{       
+{
 _print_rev_recursion(s + 1);
 _putchar(*s);
 }
