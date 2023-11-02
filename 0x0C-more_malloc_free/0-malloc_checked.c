1b34717c62a7decfeea575b19229e7b832ff2296
ashrafbel <ashrafmus2@gmial.com>
2023-11-02 15:30:14 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * *malloc_checked - malloc allocated memory
+ * @b: number of bytes
+ * Return:pointer to the allocated memory or NULL
+ */
+void *malloc_checked(unsigned int b)
+{
+int *kira;
+	kira = malloc(b);
+	if (kira == 0)
+		exit(90);
+	return (kira);
+}
