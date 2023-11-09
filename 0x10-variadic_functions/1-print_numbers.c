db95aab6006935e76614686ac006726f29b971e8
ashrafbel <ashrafmus2@gmial.com>
2023-11-09 14:34:26 -0800

    1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - Prints numbers with a specified
+ * @separator: string to separate numbers
+ * @n: Number of integers
+ * @...: Variable numbers .
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list k;
+j = 0;
+
+va_start(k, n);
+
+while (j < n)
+{
+printf("%d", va_arg(k, int));
+if (i != (n - 1) && separator != NULL)
+printf("%s", separator);
+j++;
+}
+printf("\n");
+va_end(k);
+}
+
+
