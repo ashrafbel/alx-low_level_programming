945352682f83989edd2f02552c06d0aafc4268d6
ashrafbel <ashrafmus2@gmail.com>
2023-11-17 15:50:27 -0800

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,15 +2,17 @@
 /**
  * list_len - returns the number of elements in a linked list_t list
  * @h: pointer to head node
- * Rerurn: number of ellments
+ * Return: number of ellments
  */
+
 size_t list_len(const list_t *h)
 {
 size_t L = 0;
+
 while (h != NULL)
 {
 L++;
 h = h->next;
 }
-return(L);
+return (L);
 }
