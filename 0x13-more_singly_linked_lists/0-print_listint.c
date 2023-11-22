380ec4114bb21c1a8983c9eb53e89411b8824656
ashrafbel <ashrafmus2@gmail.com>
2023-11-21 17:06:05 -0800

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,17 +4,18 @@
  * @h: pointer to the first node
  *
  * Return: the number of nodes
- */
-size_t print_listint(const listint_t *h)
+*/
+ize_t print_listint(const listint_t *h)
 {
-size_t C = 0;
+size_t k = 0;
 
 while (h)
 {
 printf("%d\n", h->n);
-C++;
+k++;
 h = h->next;
 }
-return (C);
+
+return (k);
 }
 
