ba99cfa4e62a2c8f13c77d04d78ed9c5fde31e92
ashrafbel <ashrafmus2@gmail.com>
2023-11-21 16:58:19 -0800

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,7 @@
  * print_listint - prints all the elements of a linked list
  * @h: pointer to the first node
  *
- * Return: node's number
+ * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
