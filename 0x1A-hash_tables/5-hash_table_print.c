65f3a5792b77d7915c673dbed45c46c8007f70a8
ashrafbel <ashrafmus2@gmial.com>
2024-02-02 15:28:52 -0800

    5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - hash table printed
+ * @ht: hash table to print.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+hash_node_t *Tp;
+unsigned long int a;
+
+int fpd = 0;
+if (ht == NULL)
+return;
+printf("{");
+for (a = 0; a < ht->size; a++)
+{
+Tp = ht->array[i];
+while (Tp != NULL)
+{
+if (fpd == 1)
+printf(", ");
+printf("'%s': '%s'", Tp->key, Tp->value);
+fpd = 1;
+Tp = Tp->next;
+}
+}
+printf("}\n");
+}
