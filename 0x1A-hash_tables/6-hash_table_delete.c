d2c0fad796e0ca6289e900e79e4f568b1d90d177
ashrafbel <ashrafmus2@gmial.com>
2024-02-02 15:50:25 -0800

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -5,23 +5,23 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-hash_node_t *current, *Tp;
+hash_node_t *n;
 unsigned long int a;
 
-if (ht == NULL)
+if (ht == NULL || ht->size == NULL || ht->array == NULL)
 return;
-for (a = 0; a < ht->size; a++)
-{
-current = ht->array[a];
-while (current != NULL)
-{
-Tp = current;
-current = current->next;
-free(Tp->key);
-free(Tp->value);
-free(Tp);
-}
-}
-free(ht);
-}
 
+for(a = 0; a < ht->size; a++){
+while (ht->array[a] != NULL)
+{
+n = ht->array[a]->n
+free(ht->array[a]->key);
+free(ht->array[a]->value);
+free(ht->array[a]);
+ht->array[a] = n;
+}
+}
+free(ht->array);
+ht->array = NULL;
+ht->size = 0;
+}
